8cadcd9e0e99ffb9a90db217073d0ce2288a169c
Marius <dmlita98@yahoo.com>
2025-10-12 10:25:02 +0000

    Changed the app

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 VERSION = os.getenv("APP_VERSION", "v0.0")
 MESSAGE = os.getenv("APP_MESSAGE", "Hello from Flask App!")
+NAMESPACE = os.getenv("POD_NAMESPACE", default = 'ns-read')
 
 @app.route("/")
 def home():
-    return f"{MESSAGE} - Version: {VERSION}"
+    return f"{MESSAGE} - Version: {VERSION} - Namespace: {NAMESPACE}\n"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
